842d5f893157a67108b778eb250b6f463006bf6a
Fabrizio Monge <37080465+fabriziomonge@users.noreply.github.com>
2020-12-28 09:32:24 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+import pandas as pd
+import altair as alt
+import streamlit as st
+
+from PIL import Image
+
+try:
+    image = Image.open('logo.png')
+except:
+    image = Image.open(r'C:\Users\user\Downloads\App_sim\logo.png')
+
+st.sidebar.image(image, use_column_width=True)
+
+st.sidebar.markdown('''### Seleziona i parametri del tuo grafico ''')
+
+try:
+
+    tabella = pd.read_excel(r'C:\Users\user\Downloads\App_sim\Tabella_completa.xlsx')
+    
+except:
+    
+    tabella = pd.read_excel('tabella_completa.xlsx')
+    
+    
+tabella = tabella.set_index('Unnamed: 0',1)
+
+# Qua inserisci il filtro
+
+st.title('Selettore Fondi Quantitativo')
+
+asset_sel = ['ALL']+list(tabella['Asset Class'].unique())
+
+Filtro = st.multiselect('Asset selezionata', asset_sel)
+# valore= 'Breve Medio Periodo'
+
+Filtro = list(Filtro)
+
+
+tabella_show = pd.DataFrame(columns=tabella.columns)
+if Filtro == ['ALL']:
+    tabella_show = tabella_show.append(tabella)
+else:
+    
+    for i in Filtro:
+        tabella1 = tabella.loc[tabella['Asset Class'] == i]
+        tabella_show = tabella_show.append(tabella1)
+
+
+
+
+# display(tabella.columns)
+
+#Qua inserisci i selettori
+
+indice = ['Rendimento% a 3 mesi','Rendimento% a 6 mesi','Volatilità% a 6 mesi','Max Drawdown']
+indice2 = ['Asset Class', 'Rischio', 'Orizzonte', 'Classificazione', 'Listino']
+
+assex = st.sidebar.selectbox('scegli asse x',indice, index=2)
+
+assey = st.sidebar.selectbox('scegli asse y',indice, index=1)
+
+marker = st.sidebar.selectbox('scegli marker',indice2)
+
+# assex = 'DD'
+# assey = 'roc6'
+# marker = 'Orizzonte'
+
+
+dim = st.slider('Dimensione Bolle',min_value=1, max_value=5, value=1)
+dim=dim*100
+fig1 = alt.Chart(tabella_show).mark_circle(size=dim).encode(x=assex, y=assey, color=marker,tooltip=['Asset Class', 'Categoria','Società di Gestione', 'Nome prodotto', 'ISIN', 'analisi dal:']).properties(height=500, width=700).interactive()
+
+
+fig1
+
+
+# In[1]:
+
+
+tabella_show
+
+
+# In[ ]:
+
+
+
+
